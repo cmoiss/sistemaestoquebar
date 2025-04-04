cd642ae882f71a60d29a8b27f3f76991c6bf8cdd
Caio Souza <ocaiomoises@gmail.com>
2025-04-04 11:39:00 -0300

    created - _create_generic_dataframe

--- src/service/data_frame_factory.py
+++ src/service/data_frame_factory.py
@@ -1,3 +1,12 @@
 class DataFrameFactory:
     def __init__(self):
         pass
+
+    def _create_generic_dataframe(self, *columns) -> DataFrame:
+        return DataFrame(columns=columns)
+
+    def create_dataframe_categorias(self) -> DataFrame:
+        dataframe_categorias = DataFrame(columns=["nome"])
+        return dataframe_categorias
+    
+    
